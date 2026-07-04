ff467f0cb8be3c48e864c88b4f4f01df775e401a
cuentapokego20253-rgb <cuentapokego20253@gmail.com>
2026-07-03 23:03:19 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+@bot.event
+async def on_ready():
+   print(f'Bot conectado como {bot.user}')
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+bot.run('MTUyMjc3NjYzODI2ODM3NTA5MA.GW9wZ5.lQz-1g8E2Qmh6xbKRUT3PPW7Kq2dPtUIDtGef0')
