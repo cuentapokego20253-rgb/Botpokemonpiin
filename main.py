86c5ece91cf656c6400e4e8476a3ee0cf1293e79
cuentapokego20253-rgb <cuentapokego20253@gmail.com>
2026-07-04 03:10:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import os
 import discord
 from discord.ext import commands
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "El bot está activo"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 @bot.event
 async def on_ready():
-   print(f'Bot conectado como {bot.user}')
+    print(f'Bot conectado como {bot.user}')
+
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+
+keep_alive()
 bot.run(os.environ['DISCORD_TOKEN'])
