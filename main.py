df9fe0b5ece7f1d679bf83a9dbcf2803e5bc6455
cuentapokego20253-rgb <cuentapokego20253@gmail.com>
2026-07-06 23:01:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ async def on_message(message):
     for embed in message.embeds:
         nuevo_embed = embed.copy()
          
-    if True:
+    print(DEBUG: Mensaje recibido. ¿Tiene componentes? {bool(message.components)}")
+    if message.components:
         for component in message.components:
             for child in component.children:
                 if hasattr(child, 'url') and child.url and "google" in child.url:
