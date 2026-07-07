c0ec05651485ec59b8b1334b4a97f778e7a66243
cuentapokego20253-rgb <cuentapokego20253@gmail.com>
2026-07-07 04:56:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from flask import Flask
 from threading import Thread
 
-app = Flask(__name__)
+app = Flask(_name_)
 @app.route('/')
 def home(): return "Bot activo"
 def run(): app.run(host='0.0.0.0', port=8080)
@@ -21,9 +21,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 CANALES_ESPEJO = {
     1522694582171599011: 1522738552587157536,
     1522694783280349345: 1523963115467837480,
-    1522695765301133312: 1523907438590296064,
-    1522695933031219491: 1523907697936826392,
-    1522707464150192230: 1523964283484901476,
+    1522695765901133312: 1523907433590296064,
+    1522695933031219491: 1523907607936826392,
+    1522707464450192230: 1523964283484901476,
 }
 
 PORY_ID = int(os.environ['POKEMON_BOT_ID'])
@@ -45,21 +45,13 @@ async def on_message(message):
 
         for embed in message.embeds:
             nuevo_embed = embed.copy()
+            embed_texto = str(embed.to_dict())
+            coords = re.search(r'(-?\d{1,2}\.\d{3,})(?:,|%2C)\s*(-?\d{1,3}\.\d{3,})', embed_texto)
             
-            map_url_found = None
-            if message.components:
-                for component in message.components:
-                    for child in component.children:
-                        if hasattr(child, 'url') and child.url and "google" in child.url:
-                            map_url_found = child.url
-                            break
-            
-            if map_url_found:
-                coords = re.search(r'(-?\d+\.\d+),(-?\d+\.\d+)', map_url_found)
-                if coords:
-                    lat, lon = coords.groups()
-                    api_map_url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lon}&zoom=15&size=600x300&key={MAPS_KEY}"
-                    nuevo_embed.set_image(url=api_map_url)
+            if coords:
+                lat, lon = coords.groups()
+                api_map_url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lon}&zoom=15&size=600x300&key={MAPS_KEY}"
+                nuevo_embed.set_image(url=api_map_url)
             
             canal_destino = bot.get_channel(CANALES_ESPEJO[message.channel.id])
             if canal_destino:
