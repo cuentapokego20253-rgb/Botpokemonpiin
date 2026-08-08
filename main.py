e1aa8462bce97ef7605621061d2d5f74e2b2bc29
cuentapokego20253-rgb <cuentapokego20253@gmail.com>
2026-08-08 10:13:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,6 @@ import discord
 import re
 import math
 from discord.ext import commands
-from flask import Flask
-from threading import Thread
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot activo"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
-
-keep_alive()
 
 intents = discord.Intents.default()
 intents.message_content = True
